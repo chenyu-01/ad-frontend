f6c9aaa8a9e1227b89f26bda4ce9f841a662e83d
chenyu-01 <chenyu101@outlook.com>
2024-02-02 23:44:37 +0800

    add input search box

--- src/app/property-list/page.js
+++ src/app/property-list/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { config } from "@/config";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -44,10 +45,15 @@ export default function PropertyList() {
       let response = await fetch(fetchurl);
       let data = await response.json();
       setPropertyList(data);
+      console.log(data);
     } catch (error) {
-      console.error(error);
+      console.error(error.message);
     }
   }
+  const searchProperty = (e) => {
+    e.preventDefault();
+    console.log("searching property");
+  };
 
   useEffect(() => {
     const page = searchParams.get("page");
@@ -57,9 +63,44 @@ export default function PropertyList() {
       console.log("fetching data from API");
       fetchProperties(page);
     }
-  }, [searchParams]);
+  }, [searchParams, pageNum, isRent]);
+  useEffect(() => {
+    // the first time the page is loaded, fetch data from API
+    fetchProperties(pageNum);
+  }, []);
   return (
     <div className="max-w-screen-lg container mx-auto">
+      {/* search bar */}
+      <div className="flex justify-between items-center my-5">
+        <form className="flex justify-between" onSubmit={searchProperty}>
+          <Input
+            type="text"
+            placeholder="Search"
+            className="mr-5 text-2xl"
+          ></Input>
+          <Button type="submit">Search</Button>
+        </form>
+        <div className="flex justify-end space-x-5">
+          <Button
+            onClick={(e) => {
+              setIsRent(true);
+              fetchProperties(pageNum);
+            }}
+            className={isRent ? "bg-gray-800" : "bg-gray-300"}
+          >
+            Rent
+          </Button>
+          <Button
+            onClick={(e) => {
+              setIsRent(false);
+              fetchProperties(pageNum);
+            }}
+            className={isRent ? "bg-gray-300" : "bg-gray-800"}
+          >
+            Sale
+          </Button>
+        </div>
+      </div>
       <Table className="text-2xl mb-5">
         <TableHeader>
           <TableRow>
