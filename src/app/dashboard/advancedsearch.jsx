d5b084f2448440f2e1131f7bbd271eeb3186042b
preethivenkat5 <preethivenkat5@gmail.com>
2024-02-06 22:25:38 +0800

    worked on login and dashboard

---
+++ src/app/dashboard/advancedsearch.jsx
@@ -0,0 +1,11 @@
+export default function Advanced(){
+    return(
+        <div class="form-group row">
+    <label for="town" class="col-sm-2 col-form-label">Town</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="town" placeholder="Town Here"></input>
+    </div>
+    </div>
+
+    );
+}
