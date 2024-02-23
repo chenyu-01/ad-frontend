eef3ed5e2503b0106b64809d2308dab5a2c0ec94
chenyu-01 <chenyu101@outlook.com>
2024-02-23 20:49:19 +0800

    Delete dream.jpg and update login page with next/image

--- src/app/(dashboard)/details/[id]/sections/_StampOverlay.jsx
+++ src/app/(dashboard)/details/[id]/sections/_StampOverlay.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const StampOverlay = ({ imageSrc, stampType }) => {
@@ -8,8 +9,16 @@ const StampOverlay = ({ imageSrc, stampType }) => {
 
   return (
     <div className="relative">
-      <img src={imageSrc} alt="Property" className="w-full" />
-      <img
+      <Image
+        width={`800`}
+        height={`800`}
+        src={imageSrc}
+        alt="Property"
+        className="w-full"
+      />
+      <Image
+        width={`300`}
+        height={`300`}
         src={stampSrcMap[stampType]}
         alt={stampType}
         className="absolute top-0 right-0 m-2" // Adjust margins as needed
