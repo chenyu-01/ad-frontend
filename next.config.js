225f4bdf75a785aae9d783c870bed9bd9654ffbb
chenyu-01 <chenyu101@outlook.com>
2024-02-23 19:54:27 +0800

    Remove unused dependencies and console logs

--- next.config.js
+++ next.config.js
@@ -1,25 +1,10 @@
 /** @type {import('next').NextConfig} */
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   images: {
     domains: ["localhost"],
   },
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      if (config.optimization && config.optimization.minimizer) {
-        config.optimization.minimizer.push(
-          new TerserPlugin({
-            terserOptions: {
-              compress: {
-                drop_console: true,
-              },
-            },
-          }),
-        );
-      }
-    }
-
-    return config;
+  compiler: {
+    removeConsole: true,
   },
 };
